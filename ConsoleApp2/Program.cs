8b48f0a38cf1b6181d4aeb3bf813b67b5a96a73a
rp-bim <r.polyakov@gaskar.group>
2025-01-14 17:38:53 +0300

    Other changes

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,8 +1,1 @@
-﻿git status
-// Вывод:
-// Изменён: README.md
-// Изменён: script.py
-
-git add .
-git commit -m "Обновил README и добавил новую функцию в script.py"
-git push origin main
+﻿Console.WriteLine("test");
--- ConsoleApp2/TestClass.cs
+++ ConsoleApp2/TestClass.cs
@@ -2,5 +2,12 @@ namespace ConsoleApp2;
 
 public class TestClass
 {
-    string A = "A";
+    public string testVariable { get; set; } = "A";
+    public string testVariable2 { get; set; } = "B";
+
+    TestClass(string testVariable, string testVariable2)
+    {
+        this.testVariable = testVariable;
+        this.testVariable2 = testVariable2;
+    }
 }
