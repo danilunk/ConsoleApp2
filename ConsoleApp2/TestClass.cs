ab9be0b61b0de39d47ee00aa8ea27b87865f296e
Danil <Danil@MSI>
2025-01-14 18:12:04 +0300

    del

--- ConsoleApp2/TestClass.cs
+++ ConsoleApp2/TestClass.cs
@@ -5,19 +5,4 @@ public class TestClass
     public string testVariable { get; set; } = "A";
     public string testVariable2 { get; set; } = "B";
 
-    TestClass(string testVariable, string testVariable2)
-    {
-        this.testVariable = testVariable;
-        this.testVariable2 = testVariable2;
-    }
-    
-    TestClass(string testNumber)
-    {
-        
-    }
-    
-    TestClass(int testNumber, string testVariable)
-    {
-        //
-    }
 }
