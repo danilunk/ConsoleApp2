519eaea5370c1fa73962a2c57d21467e035367e1
rp-bim <r.polyakov@gaskar.group>
2025-01-14 18:46:18 +0300

    delete all

--- ConsoleApp2/TestClass.cs
+++ ConsoleApp2/TestClass.cs
@@ -2,14 +2,6 @@ namespace ConsoleApp2;
 
 public class TestClass
 {
-    public string testVariable { get; set; } = "A";
-    public string testVariable2 { get; set; } = "B";
 
 }
 
-public class OneTwoClass
-{
-    public string testVariable { get; set; } = "A";
-    public string testVariable2 { get; set; } = "B";
-
-}
